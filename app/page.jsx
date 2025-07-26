fc4573ee56e8a53f5bda44c3dcfec41f13c7d19e
Arina Ciocildau <arina@Arinas-MacBook-Pro.local>
2025-07-26 11:35:03 +0300

    Fix: add default Page component rendering Home1MainDemoOnepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,5 @@
-export { default } from "./homes/home-1/main-demo/main-one-page/page";
+import Home1MainDemoOnepage from "./(homes)/(home-1)/(main-demo)/main-one-page/page";
+
+export default function Page() {
+  return <Home1MainDemoOnepage />;
+}
